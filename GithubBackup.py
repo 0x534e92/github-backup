804d753b4c562fe153188e90c16407b7f31f1dc8
0x534e92 <0x534e92@gmail.com>
2023-02-23 14:37:07 -0600

    Create Backups This CLI will now create a backup of all your github repos (public and private) and store them in a directory of your choosing, or in the default directory location of /Users/saad/github_backups/.
    WIP: Next, I need to also make sure to create a zip file of that directory

--- GithubBackup.py
+++ GithubBackup.py
@@ -1,12 +1,53 @@
 from github import Github
+import os
+from datetime import datetime
+import subprocess
 
 class GithubBackup:
     def __init__(self, github_token):
         self.github_token = github_token
 
     def backup_all_repos(self, backup_directory=""):
+        """
+        This method will currently clone all the repos and then generate a zip file of all the repos
+        :param backup_directory: target directory is where the backups should live, otherwise we'll default to a custom directory
+        within the user's home directory
+        :return:
+        """
+
+        # The whole purpose of this script is to get all the repos in one folder zipped up
+        # Sure, we'll re-clone them, but I think this is much cleaner than overwriting an existing folder
+        if not backup_directory:
+            backup_directory = self._create_backup_directory()
+
+
+        # Here's the basic algorithm:
+        # 1.) Go through all the user's repos
+        # 2.) Clone each repo fresh
+        # 3.) Go back to the root of `backup_directory` and create a zip of the newly created directory
+        subprocess.run(["cd", backup_directory])
+        cwd = os.getcwd()
+
+        print("Starting the cloning process...this may take a few minutes.")
+
+        commands = [f'cd {backup_directory}']
         for repo in self.client.get_user().get_repos():
-            print(repo.name)
+            commands.append(f'git clone {repo.html_url}')
+
+        command_str = ";".join(commands)
+
+        subprocess.run(command_str, shell=True, capture_output=True)
+
+        print(f"Cloned all {len(commands) - 1 } repos to the following directory: {backup_directory}")
+
+    # Set a default directory of $HOME/github_backups/current_time/
+    def _create_backup_directory(self):
+        current_time = datetime.now().strftime('%Y-%m-%d-%H_%M_%S')
+        path = os.path.expanduser("~") + os.sep + "github_backups" + os.sep + current_time
+
+        os.makedirs(path, exist_ok=False)
+
+        return path
 
     @property
     def client(self):
--- cli.py
+++ cli.py
@@ -3,23 +3,29 @@ import sys
 import argparse
 
 def repos(args):
-    gh = GithubBackup(args[0])
-    gh.backup_all_repos()
+    github_token = args[0]
 
+    try:
+        backup_dir = args[1]
+    except IndexError:
+        backup_dir = None
+
+    gh = GithubBackup(github_token=args[0])
+    gh.backup_all_repos(backup_directory=backup_dir)
 
 def main():
     parser = argparse.ArgumentParser(description = "A CLI for backing up your public and private github repos")
 
-    parser.add_argument("-r", "--repos", type=str, nargs=1,
-                        metavar="github_token", default=None,
-                        help="Backups all your github repos")
+    parser.add_argument("-r", "--repos", type=str, nargs='*',
+                        metavar=("github_token", "backup_directory"), default=None,
+                        help="Clone all repos and create a zip file of them")
 
     args = parser.parse_args()
 
-    # breakpoint()
-    print('args', args)
-
-    # This is probably over-kill, but all this is doing is routing the command to the correct method
+    """
+    This is probably over-kill, but all this is doing is routing the command to the correct method within this file. Probably a more better way to do this, but it'll be useful
+    when this is possibly extended...if I have the time :P
+    """
     command = next(iter((args.__dict__.items())))
     globals()[command[0]](command[1])
 
